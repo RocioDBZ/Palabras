0ee738afe03d13924335fdb13a12cc33ccc33d80
RocioDBZ <89035479+RocioDBZ@users.noreply.github.com>
2021-09-16 19:09:17 -0500

    Add files via upload

---
+++ Palabras/public_html/newjavascript.js
@@ -0,0 +1,19 @@
+var str = "AAADOraCAdacosaquePaSEENtuvida";
+var str = str.toUpperCase() ;
+var vecesRepetido=0;
+var letraRepetida='';
+function checkDuplicate(str){
+    for(var i = 0; i < str.length; i++){
+        var re = new RegExp("[^"+ str[i] +"]","g");
+        var repetido=str.replace(re, "");
+        
+        if(repetido.length >= vecesRepetido){
+            vecesRepetido=repetido.length;
+            letraRepetida=repetido[0];
+        }
+    }
+    return letraRepetida;
+}
+alert(checkDuplicate(str));
+
+
